68fbdc2b9b8338d7577e9df38b6c03dfb705ecf7
Pedro Lorenzo <pedroalorenzo16@gmail.com>
2026-03-11 20:10:44 -0400

    added the configuration for primeNg for our UI

--- StockGuy/src/app/app.config.ts
+++ StockGuy/src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Material from '@primeuix/themes/material';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore()]
-};
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(), providePrimeNG({ ripple: true, theme: { preset: Material } })],
+};  
